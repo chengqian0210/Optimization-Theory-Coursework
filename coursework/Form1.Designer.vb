0f16a22f53859351a58f30169cfb56a55a0a98e1
chengqian0210 <chengqian0210@outlook.com>
2020-04-22 16:19:43 +0800

    first commit

---
+++ coursework/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(43, 58)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "使用0.618方法"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(160, 60)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Location = New System.Drawing.Point(21, 12)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(270, 40)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "1.分别使用0.618方法和Fibonacci法求解下列问题" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "                min e^(-x)+x^2" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "  要求最终区间长度L<=0.2，" &
+    "取初始区间为[0，1]."
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(292, 58)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(133, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "使用 Fibonacci 方法"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(447, 60)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox3.TabIndex = 4
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox4.Location = New System.Drawing.Point(21, 112)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(290, 31)
+        Me.TextBox4.TabIndex = 5
+        Me.TextBox4.Text = "2.考虑下列问题" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "                    min 3(x^4)-4(x^3)-12(x^2)"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(43, 149)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(268, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "(1) 用牛顿法迭代3次，取初点 x(0) = -1.2;"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(447, 151)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox5.TabIndex = 8
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(43, 198)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(346, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "(2) 用割线法迭代3次，取初点 x(1) = -1.2，x(2) = -0.8;"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(447, 200)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox6.TabIndex = 10
+        '
+        'TextBox7
+        '
+        Me.TextBox7.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox7.Location = New System.Drawing.Point(21, 240)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(164, 14)
+        Me.TextBox7.TabIndex = 11
+        Me.TextBox7.Text = "3.用共轭梯度法求解下列问题" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(43, 270)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(346, 23)
+        Me.Button5.TabIndex = 12
+        Me.Button5.Text = "(3) min (x1-2)^2+2(x2-1)^2，取初始点x(1)=(1，3)T"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(447, 270)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox8.TabIndex = 13
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(43, 312)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(357, 23)
+        Me.Button6.TabIndex = 14
+        Me.Button6.Text = "(4) min 2(x1^2)+2x1x2+x2^2+3x1-4x2，取初始点x(1)=(3，4)T"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(447, 314)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox9.TabIndex = 15
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(43, 355)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(357, 23)
+        Me.Button7.TabIndex = 16
+        Me.Button7.Text = "(3) min 2(x1^2)+2x1x2+5(x2^2)，取初始点x(1)=(2，-2)T"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(447, 357)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox10.TabIndex = 17
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(573, 270)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox11.TabIndex = 18
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(573, 314)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox12.TabIndex = 19
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Location = New System.Drawing.Point(573, 357)
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(96, 21)
+        Me.TextBox13.TabIndex = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox13)
+        Me.Controls.Add(Me.TextBox12)
+        Me.Controls.Add(Me.TextBox11)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Button7 As Button
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+End Class
---
+++ coursework/Form1.vb
@@ -0,0 +1,229 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = target_fun_1(_618fun(0, 1, 0.2))
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox3.Text = target_fun_1(_fibonaccifun(0, 1, 0.2))
+    End Sub
+    Private Function target_fun_1(x As Double) As Double
+        target_fun_1 = Math.Exp(-x) + x ^ 2
+        Return target_fun_1
+    End Function
+    Private Function _618fun(a As Double, b As Double, L As Double)
+        Dim lambda, mu, a1, b1, lambda1, mu1 As Double
+        lambda = a + 0.382 * (b - a)
+        mu = a + 0.618 * (b - a)
+        While ((b - a) > L)
+            If (target_fun_1(lambda) > target_fun_1(mu)) Then
+                a1 = lambda
+                b1 = b
+                lambda1 = mu
+                mu1 = a1 + 0.618 * (b1 - a1)
+            Else
+                a1 = a
+                b1 = mu
+                mu1 = lambda
+                lambda1 = a1 + 0.382 * (b1 - a1)
+            End If
+            a = a1
+            b = b1
+            lambda = lambda1
+            mu = mu1
+        End While
+        _618fun = (b + a) / 2
+        Return _618fun
+    End Function
+    Private Function _fibonaccifun(a As Double, b As Double, L As Double)
+        Dim lambda, mu As Double
+        Dim Fib(100) As Integer
+        Dim n, k As Integer
+        Fib(0) = 1
+        Fib(1) = 1
+        Fib(2) = 2
+        For k = 3 To 100
+            Fib(k) = Fib(k - 1) + Fib(k - 2)
+            If Fib(k) > ((b - a) / L) Then
+                n = k
+                Exit For
+            End If
+        Next k
+        lambda = a + Fib(n - 2) * (b - a) / Fib(n)
+        mu = a + Fib(n - 1) * (b - a) / Fib(n)
+        For k = 2 To (n - 1)
+            If (target_fun_1(lambda) > target_fun_1(mu)) Then
+                a = lambda
+                lambda = mu
+                mu = a + (Fib(n - k) / Fib(n - k + 1)) * (b - a)
+            Else
+                b = mu
+                mu = lambda
+                lambda = a + (Fib(n - k - 1) / Fib(n - k + 1)) * (b - a)
+            End If
+        Next k
+        Return (b + a) / 2
+    End Function
+    Private Function target_fun_2(x As Double) As Double
+        target_fun_2 = 3 * (x ^ 4) - 4 * (x ^ 3) - 12 * (x ^ 2)
+        Return target_fun_2
+    End Function
+    Private Function dif(x As Double) As Double
+        Dim dx = 0.000001
+        Dim dy = target_fun_2(x + dx) - target_fun_2(x)
+        dif = dy / dx
+        Return dif
+    End Function
+    Private Function ddif(x As Double) As Double
+        Dim dx = 0.000001
+        Dim ddy = target_fun_2(x + dx) + target_fun_2(x - dx) - target_fun_2(x) * 2
+        ddif = ddy / dx / dx
+        Return ddif
+    End Function
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+    Private Function Newton(x0 As Double, k As Double) As Double
+        Dim i As Integer
+        For i = 1 To k
+            x0 = x0 - dif(x0) / ddif(x0)
+        Next i
+        Return x0
+    End Function
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox5.Text = target_fun_2(Newton(-1.2, 3))
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox6.Text = target_fun_2(secant(-1.2, -0.8, 3))
+    End Sub
+    Private Function secant(x1 As Double, x2 As Double, k As Double)
+        Dim i As Integer
+        Dim x3 As Double
+        For i = 1 To k
+            x3 = x2 - (x2 - x1) * dif(x2) / (dif(x2) - dif(x1))
+            x1 = x2
+            x2 = x3
+        Next
+        Return x2
+    End Function
+    Private Sub TextBox5_TextChanged_1(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox8.Text = target_fun_3(conjugate_gradient(2, 0, -4, 0, 4, -4, 1, 3))
+    End Sub
+
+    Private Function target_fun_3(x() As Double) As Double
+        Dim x1 = x(0)
+        Dim x2 = x(1)
+        target_fun_3 = (x1 - 2) ^ 2 + 2 * (x2 - 1) ^ 2
+        TextBox11.Text = "x1=" + x1.ToString() + ", x2=" + x2.ToString()
+        Return target_fun_3
+    End Function
+    '梯度计算
+    Private Function gradient(x1 As Double, x2 As Double, a As Double, b As Double, c As Double, d As Double, e As Double, f As Double)
+        Dim y1 = a * x1 + b * x2 + c
+        Dim y2 = d * x1 + e * x2 + f
+        Dim y(2) As Double
+        y(0) = y1
+        y(1) = y2
+        Return y
+    End Function
+    '共轭梯度法
+    Private Function conjugate_gradient(a As Double, b As Double, c As Double, d As Double, e As Double, f As Double, x1 As Double, x2 As Double)
+        '1/2xTAx+bTx+c
+        Dim g(2) As Double
+
+        Dim g11 As Double, g12 As Double, g21 As Double, g22 As Double
+        Dim A11 As Double, A12 As Double, A21 As Double, A22 As Double
+        Dim k As Integer
+        Dim lambda As Double, beta As Double
+        Dim d1 As Double, d2 As Double
+        Dim M(2) As Double
+
+        A11 = a
+        A12 = b
+        A21 = d
+        A22 = e
+        g = gradient(x1, x2, a, b, c, d, e, f) '梯度计算
+        g11 = g(0)
+        g12 = g(1)
+        If g11 = 0 Or g12 = 0 Then
+            M(0) = x1
+            M(1) = x2
+            Return M
+        Else
+            d1 = -g11
+            d2 = -g12
+
+            lambda = -(g11 * d1 + g12 * d2) / (d1 * (d1 * A11 + d2 * A21) + d2 * (d1 * A12 + d2 * A22))
+
+            x1 = x1 + lambda * d1
+            x2 = x2 + lambda * d2
+
+            For k = 1 To 100
+                g = gradient(x1, x2, a, b, c, d, e, f)
+                g21 = g(0)
+                g22 = g(1)
+                If g21 = 0 Or g22 = 0 Then Exit For
+                beta = ((d1 * A11 + d2 * A21) * g21 + (d1 * A12 + d2 * A22) * g22) / ((d1 * A11 + d2 * A21) * d1 + (d1 * A12 + d2 * A22) * d2)
+
+                d1 = -g21 + beta * d1
+                d2 = -g22 + beta * d2
+
+                lambda = -(g21 * d1 + g22 * d2) / (d1 * (d1 * A11 + d2 * A21) + d2 * (d1 * A12 + d2 * A22))
+                x1 = x1 + lambda * d1
+                x2 = x2 + lambda * d2
+
+                g11 = g21
+                g12 = g22
+            Next k
+            M(0) = x1
+            M(1) = x2
+            Return M
+        End If
+    End Function
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox9.Text = target_fun_4(conjugate_gradient(4, 2, 3, 2, 2, -4, 3, 4))
+    End Sub
+    Private Function target_fun_4(x() As Double) As Double
+        Dim x1 = x(0)
+        Dim x2 = x(1)
+        target_fun_4 = 2 * (x1 ^ 2) + 2 * x1 * x2 + x2 ^ 2 + 3 * x1 - 4 * x2
+        TextBox12.Text = "x1=" + x1.ToString() + ", x2=" + x2.ToString()
+        Return target_fun_4
+    End Function
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TextBox10.Text = target_fun_5(conjugate_gradient(4, 2, 0, 2, 10, 0, 2, -2))
+    End Sub
+    Private Function target_fun_5(x() As Double) As Double
+        Dim x1 = x(0)
+        Dim x2 = x(1)
+        target_fun_5 = 2 * (x1 ^ 2) + 2 * x1 * x2 + 5 * (x2 ^ 2)
+        TextBox13.Text = "x1=" + x1.ToString() + ", x2=" + x2.ToString()
+        Return target_fun_5
+    End Function
+End Class
